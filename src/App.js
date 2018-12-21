43ad9cd1e86ec03493ae80bb6e40030442b19328
uia97719 <rose.lyn.damo@continental-corporation.com>
2018-12-21 09:57:53 +0800

    3rd commit

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Rosa the Great!
+            Rosa the Great! yow! yow! meow!
           </p>
           <a
             className="App-link"
